3612379ac7594a07dcc7b702f8e0091f1242af17
DaniilSimankov <thedanils63@mail.ru>
2022-06-28 18:58:05 +0300

    last chance

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def wsgi_application(environ, start_response):
+def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    body = [bytes('\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
-    return body
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
